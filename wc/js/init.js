3491bd4bd73744cf030eb9ff42419b5de44fb2bf
soumitrimishra <soumitri.mishra@gmail.com>
2023-05-02 17:15:21 +0530

    wall

---
+++ wc/js/init.js
@@ -0,0 +1,13 @@
+(function($){
+  $(function(){
+
+    $('.sidenav').sidenav();
+    $('.parallax').parallax();
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
+
+var mb = document.querySelectorAll('.materialboxed');
+      M.Materialbox.init(mb,{
+
+      })
